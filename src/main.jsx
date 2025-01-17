8858f2e0fbff510b3cfa37bb31e0344bd951af21
1rajputshashank@gmail.com <hariomshashank@gmail.com>
2025-01-17 22:44:48 +0530

    remove react strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,12 @@ import { ToastContainer } from 'react-toastify'
 import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <>
     <App />
     <ToastContainer
-              position='bottom-right' autoClose={5000} hideProgressBar={false}
-              newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable 
-              pauseOnHover theme='dark'
+      position='bottom-right' autoClose={5000} hideProgressBar={false}
+      newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable 
+      pauseOnHover theme='dark'
     />
-  </React.StrictMode>,
+  </>,
 )
